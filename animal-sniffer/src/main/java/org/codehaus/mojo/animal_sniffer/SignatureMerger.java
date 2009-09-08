24fbd6d11126c20fdd31339b0fccb31fc425ece4
Stephen Connolly <stephen.alan.connolly@gmail.com>
2009-09-08 22:20:43 +0000

    adding a signature merger
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/sandbox/animal-sniffer@10640 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ animal-sniffer/src/main/java/org/codehaus/mojo/animal_sniffer/SignatureMerger.java
@@ -0,0 +1,126 @@
+package org.codehaus.mojo.animal_sniffer;
+
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2008 Kohsuke Kawaguchi and codehaus.org.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+import org.codehaus.mojo.animal_sniffer.logging.Logger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+/**
+ * Merges signature files.
+ *
+ * @author Stephen Connolly
+ */
+public class SignatureMerger
+{
+    private final Map/*<String, Clazz>*/ classes = new HashMap();
+
+    private final Logger logger;
+
+    public static void main( String[] args )
+        throws Exception
+    {
+        // TODO add command arg parsing
+//        new SignatureMerger( new FileInputStream( "signature" ), ignoredPackages,
+//                             new PrintWriterLogger( System.out ) ).process( new File( "target/classes" ) );
+    }
+
+    public SignatureMerger( InputStream[] in, OutputStream out, Logger logger )
+        throws IOException
+    {
+        this.logger = logger;
+        for ( int i = 0; i < in.length; i++ )
+        {
+            try
+            {
+                ObjectInputStream ois = new ObjectInputStream( new GZIPInputStream( in[i] ) );
+                while ( true )
+                {
+                    Clazz c = (Clazz) ois.readObject();
+                    if ( c == null )
+                    {
+                        return; // finished
+                    }
+                    Clazz cur = (Clazz) classes.get( c.name );
+                    if ( cur == null )
+                    {
+                        classes.put( c.name, c );
+                    }
+                    else
+                    {
+                        if ( !cur.superClass.equals( c.superClass ) )
+                        {
+                            // nothing we can do... this is a breaking change
+                            throw new IOException( "Class " + c.name + " has changed superclass" );
+                        }
+                        Set interfaces = new HashSet();
+                        if ( cur.superInterfaces != null )
+                        {
+                            interfaces.addAll( Arrays.asList( cur.superInterfaces ) );
+                        }
+                        if ( c.superInterfaces != null )
+                        {
+                            interfaces.addAll( Arrays.asList( c.superInterfaces ) );
+                        }
+                        Set signatures = new HashSet();
+                        signatures.addAll( cur.signatures );
+                        signatures.addAll( c.signatures );
+                        classes.put( c.name, new Clazz( c.name, signatures, cur.superClass,
+                                                        (String[]) interfaces.toArray(
+                                                            new String[interfaces.size()] ) ) );
+                    }
+                }
+            }
+            catch ( ClassNotFoundException e )
+            {
+                throw new NoClassDefFoundError( e.getMessage() );
+            }
+        }
+        ObjectOutputStream oos = new ObjectOutputStream( new GZIPOutputStream( out ) );
+        Iterator i = classes.entrySet().iterator();
+        while ( i.hasNext() )
+        {
+            Map.Entry entry = (Map.Entry) i.next();
+            logger.info( (String) entry.getKey() );
+            oos.writeObject( entry.getValue() );
+        }
+        oos.writeObject( null );   // EOF marker
+        oos.close();
+    }
+
+}
