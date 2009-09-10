e99fef3973d279e537afe97af5959a0363e6f108
Stephen Connolly <stephen.alan.connolly@gmail.com>
2009-09-10 08:13:00 +0000

    use a treemap to output the classes in name order (not strictly required, but it makes the logging of what classes were sniffed easier to follow for a human)
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/sandbox/animal-sniffer@10650 52ab4f32-60fc-0310-b215-8acea882cd1b

--- animal-sniffer/src/main/java/org/codehaus/mojo/animal_sniffer/SignatureBuilder.java
+++ animal-sniffer/src/main/java/org/codehaus/mojo/animal_sniffer/SignatureBuilder.java
@@ -43,6 +43,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
@@ -58,7 +59,7 @@ public class SignatureBuilder
 
     private final Logger logger;
 
-    private final Map classes = new HashMap();
+    private final Map classes = new TreeMap();
 
     public static void main( String[] args )
         throws IOException
