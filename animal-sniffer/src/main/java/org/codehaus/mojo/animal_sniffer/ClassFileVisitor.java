d6679ec20a09d40e5e2a09577ddc3bb8ae004712
Russell Howe <rhowe@siksai.co.uk>
2019-04-26 07:58:22 +0100

    Simplify ClassFileVisitor with a lambda expression

--- animal-sniffer/src/main/java/org/codehaus/mojo/animal_sniffer/ClassFileVisitor.java
+++ animal-sniffer/src/main/java/org/codehaus/mojo/animal_sniffer/ClassFileVisitor.java
@@ -30,7 +30,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Enumeration;
 import java.util.SortedSet;
 import java.util.TreeSet;
@@ -78,18 +77,13 @@ public abstract class ClassFileVisitor
     public void process( File[] files )
         throws IOException
     {
-        Arrays.sort( files, new Comparator<File>()
-        {
-            public int compare( File f1, File f2 )
-            {
-                String n1 = f1.getName();
-                String n2 = f2.getName();
-                // Ensure that outer classes are visited before inner classes:
-                int diff = n1.length() - n2.length();
-                return diff != 0 ? diff : n1.compareTo( n2 );
-            }
-
-        } );
+        Arrays.sort( files, (f1, f2) -> {
+            String n1 = f1.getName();
+            String n2 = f2.getName();
+            // Ensure that outer classes are visited before inner classes:
+            int diff = n1.length() - n2.length();
+            return diff != 0 ? diff : n1.compareTo( n2 );
+        });
         for (File file : files) {
             process(file);
         }
@@ -137,13 +131,11 @@ public abstract class ClassFileVisitor
     {
         try ( JarFile jar = new JarFile( file ) )
         {
-            SortedSet<JarEntry> entries = new TreeSet<>(new Comparator<JarEntry>() {
-                public int compare(JarEntry e1, JarEntry e2) {
-                    String n1 = e1.getName();
-                    String n2 = e2.getName();
-                    int diff = n1.length() - n2.length();
-                    return diff != 0 ? diff : n1.compareTo(n2);
-                }
+            SortedSet<JarEntry> entries = new TreeSet<>((e1, e2) -> {
+                String n1 = e1.getName();
+                String n2 = e2.getName();
+                int diff = n1.length() - n2.length();
+                return diff != 0 ? diff : n1.compareTo(n2);
             });
             Enumeration<JarEntry> e = jar.entries();
             while ( e.hasMoreElements() )
